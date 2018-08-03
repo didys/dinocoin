06426724043d245033344de2bed7836948e08ea6
Didy Septiyono <didy@didy.project>
2018-08-03 17:36:59 +0700

    make it better

--- depends/depbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-make -j 3 HOST=i686-pc-linux-gnu	
-make -j 3 HOST=x86_64-unknown-linux-gnu	
-make -j 3 HOST=x86_64-w64-mingw32      
-make -j 3 HOST=i686-w64-mingw32       
-make -j 3 HOST=x86_64-apple-darwin11 
